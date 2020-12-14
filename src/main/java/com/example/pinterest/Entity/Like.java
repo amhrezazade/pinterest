a0ac6d118fbb763d1e7fa7f068edf181ace99e8d
Amir <amhrezazade@gmail.com>
2020-12-15 01:33:17 +0330

    bug fix

--- src/main/java/com/example/pinterest/Entity/Like.java
+++ src/main/java/com/example/pinterest/Entity/Like.java
@@ -6,37 +6,55 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name ="likes")
-public class Like
-{
+@Table(name = "likes")
+public class Like {
 	@Id
-	@Column(name ="id")
+	@Column(name = "id")
 	private String Id;
 
-	@Column(name ="userid")
+	@Column(name = "userid")
 	private String UserId;
 
-	@Column(name ="postid")
+	@Column(name = "postid")
 	private String PostId;
 
-	@Column(name ="like")
+	@Column(name = "islike")
 	private boolean IsLike;
 
-	public Like()
-	{
-		//init class...
+	public Like() {
+		// init class...
 	}
 
-	public String getId() {return Id;}
-	public void setId(String id) {Id = id;}
+	public String getId() {
+		return Id;
+	}
 
-	public String getUserId() {return UserId;}
-	public void setUserId(String userid) {UserId = userid;}
+	public void setId(String id) {
+		Id = id;
+	}
 
-	public String getPostId() {return PostId;}
-	public void setPostId(String postid) {PostId = postid;}
+	public String getUserId() {
+		return UserId;
+	}
 
-	public Boolean getIsLike() {return IsLike;}
-	public void setIsLike(boolean islike) {IsLike = islike;}
+	public void setUserId(String userid) {
+		UserId = userid;
+	}
+
+	public String getPostId() {
+		return PostId;
+	}
+
+	public void setPostId(String postid) {
+		PostId = postid;
+	}
+
+	public boolean getIsLike() {
+		return IsLike;
+	}
+
+	public void setIsLike(boolean isLike) {
+		IsLike = isLike;
+	}
 
 }

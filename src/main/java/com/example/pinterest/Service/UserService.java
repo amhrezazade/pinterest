611d4a6a900b78ffaade71f6692f2bb63709f45d
Amir <amhrezazade@gmail.com>
2020-12-15 10:39:53 +0330

    fix

--- src/main/java/com/example/pinterest/Service/UserService.java
+++ src/main/java/com/example/pinterest/Service/UserService.java
@@ -2,9 +2,11 @@ package com.example.pinterest.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.example.pinterest.Entity.User;
 import com.example.pinterest.Model.UserModel;
+import com.example.pinterest.Model.signup;
 import com.example.pinterest.Repository.IRepositoryUser;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +18,68 @@ public class UserService
     @Autowired
     private IRepositoryUser UserRepository;
 
-      public List<UserModel> getAllUsers()
+    public String Register(signup u)
+    {
+
+      //Creating new user :
+      User user = new User();
+      user.setAddress("");
+      user.setCountry("");
+      user.setEmail(u.getEmail());
+      user.setGender(false);
+      user.setImage("image");
+      user.setPassWord(u.getPass());
+      user.setPhone("");
+      user.setUserName("");
+
+      user.setId(NextID());
+      UserRepository.save(user);
+
+      return "";
+    }
+    
+    private String NextID()
+    {
+      Random r = new Random();
+      String output = "";
+      for(int i =0;i<10;i++)
       {
-        List<User> users = UserRepository.findAll();
-        ArrayList<UserModel> Result = new ArrayList<>();
-        for (User user:(List<User>)users)
-        {
-            Result.add(new UserModel(user));
-        }
-        return Result;
+        output += r.nextInt(9);
       }
+      return output;
+    }
+
+    public static String GenerateToken()
+    {
+        Random r = new Random();
+        String chars = "";
+
+        for (char c = 'a'; c <= 'z'; c++)
+            chars += c;
+
+        for (char c = 'A'; c <= 'Z'; c++)
+            chars += c;
+
+        for (char c = '0'; c <= '9'; c++)
+            chars += c;
+
+        char[] arry = chars.toCharArray();
+        String output = "";
+        for (int i = 0; i < 32; i++)
+            output += arry[r.nextInt(chars.length())];
+
+        return output;
+    }
+
+    public List<UserModel> getAllUsers()
+    {
+      List<User> users = UserRepository.findAll();
+      ArrayList<UserModel> Result = new ArrayList<>();
+      for (User user:(List<User>)users)
+      {
+          Result.add(new UserModel(user));
+      }
+      return Result;
+    }
       
 }
--- src/main/java/com/example/pinterest/controllers/AccountController.java
+++ src/main/java/com/example/pinterest/controllers/AccountController.java
@@ -1,20 +1,29 @@
 package com.example.pinterest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
+import com.example.pinterest.Entity.User;
 import com.example.pinterest.Model.UserModel;
 import com.example.pinterest.Model.signup;
-
+import com.example.pinterest.Service.UserService;
 
 @Controller
-@RequestMapping({ "/accunt" })
+@RequestMapping({ "/acc" })
 public class AccountController {
+
+    @Autowired
+    private UserService Userservice;
 
     @RequestMapping({ "/login1" })
     public String login(final Model model) {
@@ -22,25 +31,35 @@ public class AccountController {
     }
 
     @RequestMapping({ "/login2" })
-    public String login(final Model model,UserModel u) {
-       
+    public String login(final Model model, UserModel u) {
+
         return "login";
     }
 
     @GetMapping("/signup")
-    public String SignUp(Model model){
+    public String SignUp(Model model) {
 
         model.addAttribute("UserSignUpModel", new signup());
         return "signup";
     }
 
-
     @PostMapping("/signupUser")
-    public String SignUp(@ModelAttribute signup u, Model model){
-        System.out.println(u.getEmail());
-        System.out.println(u.getPass());
-        System.out.println(u.getPass2());
-        return "index";
+    public String SignUp(@ModelAttribute signup data, Model model, HttpServletResponse response) {
+
+
+
+        //Register to database :
+        Userservice.Register(user);
+
+        // create a cookie
+        Cookie cookie = new Cookie("token", "");
+        // add cookie to response
+        response.addCookie(cookie);
+
+        System.out.println("RegisterOK");
+
+        return "redirect:/index";
     }
+
 
 }
